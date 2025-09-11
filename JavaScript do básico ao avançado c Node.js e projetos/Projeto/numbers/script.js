e8bec5c4539609c20f3b361bc03cca3b4c8580f8
Pedro Henrique P. Rodrigues <110696250+R0DRlGUES@users.noreply.github.com>
2025-09-11 13:18:57 -0300

    curso de javaScript udemy

---
+++ JavaScript do básico ao avançado c Node.js e projetos/Projeto/numbers/script.js
@@ -0,0 +1,7 @@
+
+// um número é um número independente de ser positivo, negativo, inteiro ou decimal.
+console.log(typeof 123); // number
+console.log(typeof -123); // number
+console.log(typeof 123.2); // number
+
+

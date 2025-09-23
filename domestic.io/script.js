2262f8b451be39da84fa1081de4063b65ce75b65
R0DRlGUES <pedrohenrique.p.r@outlook.com>
2025-09-23 16:59:40 -0300

    add

--- domestic.io/script.js
+++ domestic.io/script.js
@@ -1,22 +1,49 @@
-let tarefasDomesticas = [
-  "Lavar a louça",
-  "Varrer a casa",
-  "Passar pano no chão",
-  "Limpar o banheiro",
-  "Arrumar a cama",
-  "Lavar roupa",
-  "Dobrar roupa",
-  "Organizar a cozinha",
-  "Tirar o lixo",
-  "Regar as plantas",
-  "Limpar a Gaiola do thuthucos"
-];
+const formulario = document.getElementById("formNovaTarefa");
+const inputNome = document.querySelector("#inputNomeTarefa");
+const txaDescricao = document.querySelector("#textareaDescricao");
+const btAddTarefa = document.querySelector("#buttonAdicionarTarefa");
+const lista = document.getElementById("listaTarefas");
+
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const tituloTarefa = inputNome.value.trim();
+  const descricaoTarefa = txaDescricao.value.trim();
+
+  if (tituloTarefa === "") {
+    console.log("Erro: título vazio");
+    return;
+  }
+
+  const itemTarefa = document.createElement("li");
+  itemTarefa.innerHTML = `<strong>${tituloTarefa}</strong><br><small>${descricaoTarefa}</small>`;
+
+  lista.appendChild(itemTarefa);
+
+  // limpar campos
+  inputNome.value = "";
+  txaDescricao.value = "";
+});
+
+// let tarefasDomesticas = [
+//   "Lavar a louça",
+//   "Varrer a casa",
+//   "Passar pano no chão",
+//   "Limpar o banheiro",
+//   "Arrumar a cama",
+//   "Lavar roupa",
+//   "Dobrar roupa",
+//   "Organizar a cozinha",
+//   "Tirar o lixo",
+//   "Regar as plantas",
+//   "Limpar a Gaiola do thuthucos"
+// ];
+
+let tarefasDomesticas =[];
 const listaEmbaralhada = shuffler(tarefasDomesticas);
 let listaDoPedro = [];
 let listaDajulia = [];
-
-let meioDaLista;
-
+//  let meioDaLista;
 
 function shuffler(arr){
     let novoArr = [...arr];
@@ -32,14 +59,3 @@ listaDoPedro = listaEmbaralhada.slice(0,Math.floor(listaEmbaralhada.length / 2))
 listaDajulia = listaEmbaralhada.slice(Math.floor(listaEmbaralhada.length / 2));
 console.log("Lista do Pedro = " + listaDoPedro);
 console.log("Lista da Julia = " + listaDajulia);
-// -------------------------
-// -------------------------
-// -------------------------
-// function embaralhar(arr){
-//   let novo2arr = [...arr]
-//   for(let i = arr.length -1;i > 0; i--){
-//     let a = Math.floor(Math.random() * (i + 1));
-//     [novo2arr[i] , novo2arr[a]] = [novo2arr[a] , novo2arr[i]];
-//   }
-//   return novo2arr;
-// }

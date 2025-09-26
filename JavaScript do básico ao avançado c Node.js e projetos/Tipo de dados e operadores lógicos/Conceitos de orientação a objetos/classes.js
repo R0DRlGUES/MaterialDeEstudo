f83704fbef5e5cdcb69a826cba26a6cab120c1cb
R0DRlGUES <pedrohenrique.p.r@outlook.com>
2025-09-26 12:50:23 -0300

    maneiras de criar classes

--- JavaScript do básico ao avançado c Node.js e projetos/Tipo de dados e operadores lógicos/Conceitos de orientação a objetos/classes.js
+++ JavaScript do básico ao avançado c Node.js e projetos/Tipo de dados e operadores lógicos/Conceitos de orientação a objetos/classes.js
@@ -25,4 +25,40 @@ function cirarCachorro(raca,patas,cor,){
 }
 
 let doberman = cirarCachorro('doberman', 4, "preto");
-console.log(doberman.latir());
+//  console.log(doberman.latir());
+
+// new Object()
+
+function Cachorro(raca,patas,cor){
+    this.patas = patas; 
+    this.raca = raca;   
+    this.cor = cor; 
+}
+
+let husky = new Cachorro('husky',4,'cinza e branco');
+
+Cachorro.prototype.uivar = function(){
+    console.log("Auuuuuuuu");
+}
+Cachorro.prototype.latir = function(){
+    console.log("Au au");
+}
+
+husky.uivar();
+husky.latir();
+
+class Gato{
+    constructor(raca,cor,patas){
+        this.raca = raca;
+        this.cor = cor;
+        this.patas = patas;
+    }
+    miar(){
+        console.log('Miau miaaaaaauuu!!');
+    }
+}
+Gato.prototype.rabo = true;
+let siames = new Gato('siames','amarelo e branco',4);
+
+console.log(siames);
+console.log(siames.rabo);
